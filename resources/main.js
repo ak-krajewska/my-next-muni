59998ee928b7c121d25e935b55929b1db3b94c53
AK Krajewska <akrajewska@gmail.com>
2017-09-05 16:05:05 -0700

    show next J for hardcoded stop ID

--- resources/main.js
+++ resources/main.js
@@ -1,7 +1,19 @@
 // do some stuff on load
 $(document).ready(function(){
     //display the next J times
-    $('#nextJ').append('<p>This is the J</p>');        
+         
+     
+     var nextBusURL = "http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=sf-muni&stopId=16213";
+     $.getJSON(nextBusURL, function(data){
+         //console.log(data); 
+         var nextJ = data.predictions.direction.prediction[0].minutes;
+         console.log(nextJ);
+         $('#nextJ').append('<p>Next J is in ' + nextJ + ' minutes.</p>');   
+     });
+    
+        
     
     });
+
+
 
